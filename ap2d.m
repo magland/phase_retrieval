e241ee73e9b16e8b8e33d585286cefce4ef7b0bb
Jeremy Magland <jeremy.magland@gmail.com>
2015-10-03 05:46:23 -0400

    improve?

--- ap2d.m
+++ ap2d.m
@@ -1,9 +1,13 @@
-function f=ap2d(xx,yy,u,opts)
+function [f,resid]=ap2d(xx,yy,u,opts)
 
 if (nargin==0) test_ap2d; return; end;
 
 alpha1=opts.mu1/opts.beta;
-alpha2=opts.mu2*alpha1/(opts.mu1+opts.mu2*alpha1);
+if (~isinf(opts.mu2))
+    alpha2=opts.mu2*alpha1/(opts.mu1+opts.mu2*alpha1);
+else
+    alpha2=1;
+end;
 if (isfield(opts,'f_ref'))
     fhat_ref=fftb(opts.f_ref);
 end;
@@ -16,6 +20,7 @@ resid1=ones(1,opts.numit)*inf;
 resid2=ones(1,opts.numit)*inf;
 residsum=ones(1,opts.numit)*inf;
 error=ones(1,opts.numit)*inf;
+diffs=ones(1,opts.numit)*inf;
 
 fig_timer=tic;
 for it=1:opts.numit
@@ -41,7 +46,7 @@ for it=1:opts.numit
         imagesc(g_next_registered); colormap('gray');
         title('Recon');
         subplot(1,4,2);
-        semilogy(1:opts.numit,residsum,'k',1:opts.numit,resid1,'b',1:opts.numit,resid2,'r',2:opts.numit,1e3*abs(diff(residsum)),'m',1:opts.numit,error,'c');
+        semilogy(1:opts.numit,residsum,'k',1:opts.numit,resid1,'b',1:opts.numit,resid2,'r',1:opts.numit,diffs,'m',1:opts.numit,error,'c');
         legend('resid sum','resid 1','resid 2','1000 x diff resid sum','error');
         title('Residuals');
         if (isfield(opts,'f_ref'))
@@ -62,11 +67,14 @@ for it=1:opts.numit
 
     end;
 
+    diffs(it)=sum(abs(g_current(:)-g_next(:)).^2);
     g_current=g_next;
 
 end
 
 f=g_current;
+
+resid=residsum(opts.numit);
 
 end
 
@@ -142,12 +150,12 @@ rng(1);
 
 close all;
 
-opts.mu1=1; opts.mu2=1; opts.beta=opts.mu1*1.5;
+opts.mu1=1; opts.mu2=1; opts.beta=opts.mu1*1.1;
 opts.numit=50000;
-oversamp=2;
-N=500;
+oversamp=1;
+N=40;
 rand_init_factor=1;
-example_num=2;
+example_num=4;
 
 [xx,yy]=ndgrid(linspace(-oversamp,oversamp,N),linspace(-oversamp,oversamp,N));
 
@@ -161,22 +169,63 @@ elseif example_num==2
     f_exact=F1+F2;
 elseif example_num==3
     f_exact=(abs(xx-0.1)<0.6).*abs(xx) + create_gaussian(xx,yy,0.2) + rand(size(xx))*0.02;
+elseif example_num==4
+    f_exact=zeros(size(xx));
+    for kk=1:10
+        cc=(rand(2,1)*2-1)*0.7;
+        rr=(rand*2-1)*0.25;
+        f_exact=f_exact+create_gaussian(xx-cc(1),yy-cc(2),rr/2).*((xx-cc(1)).^2+(yy-cc(2)).^2<=rr^2);
+    end;
+    f_exact=f_exact+create_gaussian(xx,yy,0.5);
+    f_exact=f_exact+rand(size(xx))*0.02;
 end;
 
 f_exact=(f_exact).*(abs(xx)<=1).*(abs(yy)<=1);
 
-%f_exact= real(ifftb(gaussian_apodize(fftb(f_exact),30)));
+f_exact= real(ifftb(gaussian_apodize(fftb(f_exact),6)));
 xx=xx/2; yy=yy/2;
 
 u=abs(fftb(f_exact));
-u(u(:)<max(u(:))*0.1)=0;
+%u(u(:)<max(u(:))*0.05)=0;
+length(find(u(:)>0))
 figure; hist(log10(u(:)),1000); xlim([-10,10]);
 
 opts.f_ref=f_exact;
-opts.f_init=f_exact + pi*(rand(size(f_exact))*2-1) * rand_init_factor;
 opts.fig=figure; set(opts.fig,'position',[100,100,1600,400]);
 figure; imagesc(opts.f_ref); colormap('gray');
-f=ap2d(xx,yy,u,opts);
+
+opts.numit=2000
+best_f=zeros(size(f_exact));
+best_resid=inf;
+fA=figure;
+recons={};
+for j=1:50
+opts.f_init=f_exact + pi*(rand(size(f_exact))*2-1) * rand_init_factor;
+[f,resid]=ap2d(xx,yy,u,opts);
+f=register_with_reference(f,f_exact);
+recons{j}=f;
+if (resid<best_resid)
+    best_f=f;
+    best_resid=resid;
+    figure(fA); imagesc(best_f); colormap('gray'); title(sprintf('%g',best_resid));
+end;
+fprintf('%d: %g\n',j,resid);
+end;
+
+AA=zeros(length(recons),length(recons));
+for j=1:length(recons)
+    for k=1:length(recons)
+        tmp1=recons{j};
+        tmp2=recons{k};
+        AA(j,k)=sum(tmp1(:).*tmp2(:));
+    end;
+end;
+
+figure; imagesc(AA); colormap('gray');
+
+%opts.numit=500;
+%opts.f_init=best_f;
+%[f,resid]=ap2d(xx,yy,u,opts);
 
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
